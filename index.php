5e0eed7cdf384ced1ac57a08a065386aaba83750
Vincent Tran <hiimvincent22@gmail.com>
2024-04-23 13:50:39 -0400

    i like boys

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>  
+    <link rel="stylesheet" href="styles/styles.css">
+    <title>Recipe Book - Home Page</title>
+</head>
+<body>
+    <header>
+        <h1>Welcome to My Recipe Book Page</h1>
+    </header>
+    
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="create_recipe.php">Create Recipe</a></li>
+            <li><a href="search_recipe.php">Search a Recipe</a></li>         
+            <li><a href="ingredients_management.php">Ingredients Management</a></li>
+        </ul>
+    </nav>
+
+    <section>
+        <h1>Welcome to Steam!</h1>
+        <p>This is a simplified database representation of the existing online store, meant to replicate and simulate its features to its bare bones</p>
+
+        
+    </section>
+
+    <footer>
+        <p>&copy; 2024 NotSteam by Vincent Tran and Sean Bolles</p>
+    </footer>
+</body>
+</html>
